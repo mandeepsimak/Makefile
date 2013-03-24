88698c29df2a99fa505915a7a77c8d0ca11f88ec
megha55 <meghasimak@gmail.com>
2013-03-24 13:33:19 +0530

    Main file

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/**
+ *       \file       main.cpp
+ *
+ *       \brief      Main file
+ *
+ *       \date       Sunday 24 March 2013 01:23:36  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+/**
+ *  Include local header file 
+ */
+
+#include "header.h"
+
+/**
+ *  Main method 
+ */
+
+int main(void)
+{
+    cout << "\n Hello..!! \n" << endl;
+
+    return 0;
+}
