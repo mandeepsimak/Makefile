87f8393bc240465dc687588a45c6842a8538cef3
megha55 <meghasimak@gmail.com>
2013-03-24 13:33:02 +0530

    header file

---
+++ header.h
@@ -0,0 +1,17 @@
+/**
+ *       \file       header.h
+ *
+ *       \brief      Include header files, declare constants, etc.
+ *
+ *       \date       Sunday 24 March 2013 01:21:33  IST\n
+ *       Compiler    g++
+ *
+ *       \author     Mandeep Kaur, meghasimak@gmail.com
+ *       License     GNU General Public License
+ *       \copyright  Copyright (c) 2013, GreatDevelopers
+ *                   https://github.com/GreatDevelopers
+ */
+
+#include <iostream>
+
+using namespace std;
